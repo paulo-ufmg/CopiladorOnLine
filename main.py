86f11ea561a0eb95992c4c84c7236c12196067a0
Mekerjunior <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>
2024-03-17 18:35:07 -0300

    Update

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+# from flask import Flask, request, render_template, jsonify
+# from flask_cors import CORS
+# import subprocess
+
+# # Para instalar os pacotes use pip install -r requirements.txt
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/execute', methods=['POST'])
+# def execute():
+#     data = request.get_json()
+#     code = data['code']
+    
+#     try:
+#         result = subprocess.run(['python', '-c', code], capture_output=True, text=True)
+#         output = result.stdout.strip()
+#         return jsonify({'output': output})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
+
+# if __name__ == '__main__':
+#     app.run(debug=False)
+
+
 from flask import Flask, request, render_template, jsonify
-from flask_cors import CORS
 import subprocess
 
-# Para instalar os pacotes use pip install -r requirements.txt
-
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
@@ -24,4 +49,4 @@ def execute():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000)
